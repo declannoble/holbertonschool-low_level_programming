372f22fb434371dbbd99ba21d343fc9b4e6a9786
declannoble <declanjnoble@hotmail.com>
2022-02-03 22:10:06 -0800

    altered text

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
-
 /**
- * main - prints the lower case alphabet, but not q or e, and a new line
+ * main - use putchar to print alphabet sans q+e
  *
- * Return: returns 0
+ * Return: Always 0 
  */
 int main(void)
 {
-	char c
+	int ch;
 
-	for (c = 'a'; c <= 'z'; ++c)
-		if (c != 'e' && c != 'q')
-			putchar(c);
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
+
