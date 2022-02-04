f8dedcb6174d3e752b5675bbbf9a36bb3ee218cd
declannoble <declanjnoble@hotmail.com>
2022-02-03 18:49:53 -0800

    altered to adhere to betty style

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main- defines entry point
+ *
+ *Return: 0 at the end of the function
+ *
+ */
+
+int main(void)
+{
+char ch;
+
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar ('\n');
+return (0);
+}
