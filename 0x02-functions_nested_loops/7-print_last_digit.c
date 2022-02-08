0924f1db0f201d7b4ef9d276725e0bedb059c5b2
decalnnoble <declanjnoble@hotmail.com>
2022-02-08 01:26:33 -0800

    betty styling

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,20 +1,27 @@
 #include "main.h"
 
 /**
- *print_last_digit - prints last digit of number
+ * print_last_digit - prints last digit
  *
- *@c: number being printed
  *
- * Return: always 0
+ * @c: number to be printed
+ *
+ * Return: success
  */
+
+
 int print_last_digit(int c)
 {
-	int a;
+	int i;
+
 	if (c < 0)
 		c = -c;
-	a = c % 10;
-	if (a < 0)
-		a = -a;
-	_putchar('0' + a);
-	return (a);
+
+	i = c % 10;
+
+	if (i < 0)
+		c = -c;
+	_putchar('0' + i);
+
+	return (i);
 }
