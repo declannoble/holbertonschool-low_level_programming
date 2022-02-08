4df604bc5721d0569b86873eab5e9301d4b754c8
decalnnoble <declanjnoble@hotmail.com>
2022-02-07 18:21:02 -0800

    adding files

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - short description here
+ *
+ * Description: long description here
+ *
+ * Return: explain what happens on return here
+ */
+
+int main(void)
+{
+	char c[] = "_putchar";
+
+	printf("%s\n", c);
+
+	return (0);
+}
