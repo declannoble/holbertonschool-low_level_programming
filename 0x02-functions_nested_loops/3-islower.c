9f5bbe6df71373f80aca46c6da918c0e165222e2
decalnnoble <declanjnoble@hotmail.com>
2022-02-07 22:08:24 -0800

    first commit

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower- checks to see if lowercase or not
+ *
+ * return- 1 if lowercase and 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
