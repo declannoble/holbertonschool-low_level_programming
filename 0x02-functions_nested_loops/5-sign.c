c4bb4707d6d67ef536a642e2e396da8fef697c66
decalnnoble <declanjnoble@hotmail.com>
2022-02-07 23:07:21 -0800

    first commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *print_sign - prints sign of a number
+ *
+ *@n: character being checked
+ *
+ *Return: 1 if greater than zero, 0 if equal and -1 is less than 0
+ *
+ */
+int print_sign(int n)
+{
+
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
