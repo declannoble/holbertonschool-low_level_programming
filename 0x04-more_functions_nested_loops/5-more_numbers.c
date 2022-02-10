eb1d32c3b38fc36115fc99df43c123fe78ea111d
decalnnoble <declanjnoble@hotmail.com>
2022-02-10 01:33:43 -0800

    please work

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers- print 10 rows of numbers 0-14
+ *
+ * Return: 0 
+ */
+
+void more_numbers(void)
+{
+	int n;
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+
+		_putchar('\n');
+	}
+}
