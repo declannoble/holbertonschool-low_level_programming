edd5a162ae74c89501f11c59935f9854993d1e15
decalnnoble <declanjnoble@hotmail.com>
2022-02-09 22:26:03 -0800

    first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - checks for uppercase letter
+ * @c: character being checked
+ * Return: 1 if uppercase and 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c <= 'A' && c >= 'Z')
+		c = 1;
+	else
+		c = 0;
+	return (c);
+}
