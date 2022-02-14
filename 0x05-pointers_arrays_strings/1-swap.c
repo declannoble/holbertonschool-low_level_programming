d302b73d37f811ea2b12a85efcce718b5bde927c
decalnnoble <declanjnoble@hotmail.com>
2022-02-13 16:41:48 -0800

    first commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ * swap_int - swaps value of integers
+ *
+ *@a: value being swapped
+ *@b: value being swapped
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,6 +1,7 @@
 #ifndef header_file
 #define header_file
 void reset_to_98(int *n);
+oid swap_int(int *a, int *b);
 int main(void);
 int _putchar(char c);
 #endif
