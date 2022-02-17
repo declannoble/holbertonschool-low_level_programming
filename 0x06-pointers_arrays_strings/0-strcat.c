a32d57fc654bd8ed46440194d5853df6fb9fa5e0
decalnnoble <declanjnoble@hotmail.com>
2022-02-16 16:46:14 -0800

    first commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates strings
+ * @dest: string
+ * @src: string to be copied
+ *
+ * Return: 0 success
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 1;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	return (src);
+}
