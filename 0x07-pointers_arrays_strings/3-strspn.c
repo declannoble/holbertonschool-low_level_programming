04303c47025bcfc25a5a0646ffd42678b8bf41ba
decalnnoble <declanjnoble@hotmail.com>
2022-02-20 23:11:33 -0800

    added

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strspn - gets length of prefixed substring
+ *@s: pointer
+ *@accept: pointer
+ * Return: 0 success
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return(s);
+	return ('\0');
+}
