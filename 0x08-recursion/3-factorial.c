4b49060bcd9ca39493e97c029936ee3ef7602326
decalnnoble <declanjnoble@hotmail.com>
2022-02-23 03:20:40 -0800

    bulk commit

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
-
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to be investigated
+ * Return: factorial of n
+ */
 int factorial(int n)
 {
 	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (1);
-	return(n * factorial(n - 1));
-	
-}
-
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int r;
-
-    r = factorial(1);
-    printf("%d\n", r);
-    r = factorial(5);
-    printf("%d\n", r);
-    r = factorial(10);
-    printf("%d\n", r);
-    r = factorial(-1024);
-    printf("%d\n", r);
-    return (0);
+	return (n * factorial(n - 1));
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,39 +1,34 @@
 #include <stdio.h>
+/**
+ * find_sqrt - tests if the y * y is equal to n
+ * @n: an incrementing natural number
+ * @sqrt: square root
+ * Return: num + 1 as an integer
+ */
 int find_sqrt(unsigned int n, int sqrt)
 {
 	unsigned int i = sqrt * sqrt;
+
 	if (n == i)
-		return(sqrt);
+		return (sqrt);
 	if (n < i)
-		return(-1);
+		return (-1);
 	else
-		return(find_sqrt(n , 1+ sqrt));
+		return (find_sqrt(n, 1 + sqrt));
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to be investigated
+ * Return: natural square root of n
+ */
 
 int _sqrt_recursion(int n)
 {
 	if (n == 1)
-		return(1);
+		return (1);
 	if (n <= 0)
-		return(-1);
+		return (-1);
 
-	return(find_sqrt(n , 2));
+	return (find_sqrt(n, 2));
 
-}
-int main(void)
-{
-	int r;
-       r = _sqrt_recursion(1);
-	  printf("%d\n", r);
-    r = _sqrt_recursion(1024);
-    printf("%d\n", r);
-    r = _sqrt_recursion(16);
-    printf("%d\n", r);
-    r = _sqrt_recursion(17);
-       printf("%d\n", r);
-    r = _sqrt_recursion(25);
-    printf("%d\n", r);
-    r = _sqrt_recursion(-1);
-       printf("%d\n", r);
-    return (0);
 }
