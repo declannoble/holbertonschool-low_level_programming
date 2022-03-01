c5553be4984766c91a1a0ba4977e58d27cfad418
declannoble <declanjnoble@hotmail.com>
2022-02-28 19:57:31 -0800

    adhering to betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,17 +8,17 @@
  */
 char *create_array(unsigned int size, char c)
 {
-  unsigned int i;
-  char *array;
+unsigned int i;
+char *array;
 
-  array = malloc(sizeof(char) * size);
+array = malloc(sizeof(char) * size);
 
-  if (array == NULL)
-    return (0);
+if (array == NULL)
+return (0);
 
-  for (i = 0; i < size; i++)
-    {
-      array[i] = c;
-    }
-  return (array);
+for (i = 0; i < size; i++)
+{
+array[i] = c;
+}
+return (array);
 }
