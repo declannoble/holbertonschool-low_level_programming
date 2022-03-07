061e805da3e3c889ed25ffd0130df7462b4a7ea8
declannoble <declanjnoble@hotmail.com>
2022-03-06 18:54:55 -0800

    altered text

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 		printf("Age: %f\n", (*d).age);
 
 		if ((*d).owner == NULL)
-			printf("Owner: (nil)\n");
+k			printf("Owner: (nil)\n");
 		else
 			printf("Owner: %s\n", (*d).owner);
 
