9b5ffa3a205b45ccf716ecbcc4d192918ed7ba0f
declannoble <declanjnoble@hotmail.com>
2022-03-08 23:30:19 -0800

    initial commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - prints name
+ * @name: the name
+ * @f: pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+
+		f(name);
+}
