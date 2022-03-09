f10b17971f439509b330ac13e461b4cef218711e
declannoble <declanjnoble@hotmail.com>
2022-03-09 00:10:24 -0800

    initial commit

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - executes function as a parameter on each element
+ * @size: size of array
+ * @array: array
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	while (i > size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
