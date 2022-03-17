19e8c5ba94c80412acce366240d1ba857864984a
declannoble <declanjnoble@hotmail.com>
2022-03-17 01:16:45 -0700

    altered condition

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,17 +6,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	int c = 0;
+	int count = 0;
 
 	while (h != NULL)
 	{
-	if (h->str == NULL)
-		printf("[0] (nil)\n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 	else
 		printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
-	c++;
+	count++;
 	}
-	return (c);
+	return (count);
 
 }
