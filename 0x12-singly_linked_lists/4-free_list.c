1849e3972191ee78b30b3448774f8b4a987f6100
declannoble <declanjnoble@hotmail.com>
2022-03-17 01:51:12 -0700

    initial commit

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: pointer
+ * Return: 0 success
+ */
+void free_list(list_t *head)
+{
+	list_t *dec;
+
+	while (head != NULL)
+	{
+		dec = head;
+		head = head->next;
+		free (dec->str);
+		free (dec);
+	}
+}
