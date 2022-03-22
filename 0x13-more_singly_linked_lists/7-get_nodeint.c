b73873724f770df691e1a893be35aa3b043e3058
declannoble <declanjnoble@hotmail.com>
2022-03-21 18:21:38 -0700

    initial commit

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *get_nodeint__index- deletes nth node of linked list
+ *@head: pointer to head node
+ *@index: index of nodes
+ *Return: NULL if non existant, otherwise return node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp = head;
+	unsigned int n = 0;
+
+	while (tmp != NULL)
+	{
+		if (n == index)
+			return (temp);
+		n++;
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
