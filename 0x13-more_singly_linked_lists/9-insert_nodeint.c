87c2ac00c39b6f138b2f0ae9f9a86a4739de5580
declannoble <declanjnoble@hotmail.com>
2022-03-21 21:57:26 -0700

    removed extra initialisation

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *current, *newnode = *head;
+	listint_t *current, *newnode;
 
 	newnode = malloc(sizeof(listint_t));
 
