c3b02152d5758e2a2220d00d50c6c23debd49d45
declannoble <declanjnoble@hotmail.com>
2022-03-21 18:23:41 -0700

    fixed declaration

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (tmp != NULL)
 	{
 		if (n == index)
-			return (temp);
+			return (tmp);
 		n++;
 		tmp = tmp->next;
 	}
