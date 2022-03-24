4eea3fd6699aa3efd2fa0a6b3999b34a1d67e6c6
declannoble <declanjnoble@hotmail.com>
2022-03-23 20:25:50 -0700

    fixed logical operands

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,13 +12,16 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	while (b[i] != '\0')
+	while (b[i] == '0' || b[i] == '1')
 	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-
 		result <<= 1;
 		result = result + b[i] - '0';
+		i++;
+	}
+	while (b[i] != '\0')
+	{
+		if (b[i] != '1' || b[i] != '\0')
+			return (0);
 		i++;
 	}
 	return (result);
