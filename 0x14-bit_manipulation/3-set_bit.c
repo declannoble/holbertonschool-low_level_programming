a9e1c9f4511bf8252dfac6603d912c9f256cae65
declannoble <declanjnoble@hotmail.com>
2022-03-24 04:22:51 -0700

    changed bits to test

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,8 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	/* if n is larger than 32 bits error occurs */
-	if (n == NULL || index > 31)
+	/* if n is larger than 64 bits error occurs */
+	if (n == NULL || index > 63)
 		return (-1);
 	/* shifts starting bit left to compare with given index */
 	*n = *n | 1 << index;
