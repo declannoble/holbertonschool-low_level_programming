33b4bf1a09ab37eae462f8b7463a5cd3233464b3
declannoble <declanjnoble@hotmail.com>
2022-03-24 05:24:03 -0700

    changed to recursion

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,31 +1,14 @@
 #include "main.h"
 /**
- * print_binary - prints binary representation of number
- *@n: number
- *Return: void
+ * print_binary - convert a given int to binary
+ * @n:number
+ * Return: nothing
  */
+
 void print_binary(unsigned long int n)
 {
-	int i = 31;
-	int b = 0;
-	int result = 0;
+	if (n > 1)
+		print_binary(n >> 1);
 
-	while (i > 0 && result == 0)
-	{
-		/* shifts starting bit all the way to the right and compares */
-		b = (n >> i) & 1;
-		/* if the result of comparing bits is 1 */
-		if (b == 1)
-		{
-			result = 1;
-		}
-		else
-			i--;
-	}
-	while (i >= 0)
-	{
-		b = (n >> i) & 1;
-		i--;
-		_putchar(b + '0');
-	}
+	_putchar((n & 1) + '0');
 }
