67b87ec1e10773195ec9e02679231f367c1cc31f
declannoble <declanjnoble@hotmail.com>
2022-03-23 20:44:06 -0700

    added brackets

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,25 @@
 #include "main.h"
 /**
- * binary_to_uint - convers binary strings into unsigned int
- *@b: string to be converted
- *Return: unsigned int or 0 if error occurs
+ * binary_to_uint - convert a string of binary to unsigned int
+ * @b: the string to convert
+ * Description: convert a string of binary numbers to unsigned int
+ * Return: converted number of 0 if error occurs
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
 	unsigned int result = 0;
+	int i = 0;
 
 	if (b == NULL)
 		return (0);
-	while (b[i] != '0' && b[i] != '1')
-		return (0);
 
-	while (b[i] == '0' || b[i] == '1')
+	while (b[i] != '\0')
 	{
-		result <<= 1;
-		result = result + b[i] - '0';
+		if (b[i] != '0' &&  b[i] != '1')
+			return (0);
+
+		result = result << 1;
+		result = (result + b[i]) - '0';
 		i++;
 	}
 	return (result);
