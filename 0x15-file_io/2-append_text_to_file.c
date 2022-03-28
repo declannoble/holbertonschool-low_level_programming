6cee77203aff1170299b073fc65ba56599bc3626
declannoble <declanjnoble@hotmail.com>
2022-03-27 22:41:06 -0700

    initial commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -33,22 +33,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_APPEND | O_WRONGLY)
+	fd = open(filename, O_APPEND | O_WRONLY);
 
 	if (fd == -1)
 		return (-1);
-
-	if (text_content == NULL)
-		return (1);
-
-	len = _strlen(text_content);
-	bytwrite = write(fd, text_content, len);
-
-	if (bytwrite == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		len = _strlen(text_content);
+		bytwrite = write(fd, text_content, len);
+		if (bytwrite == -1)
+			return (-1);
 	}
+
 	close(fd);
 	return (1);
 }
