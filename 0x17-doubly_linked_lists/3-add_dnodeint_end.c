b3b20bf890efc96f38f25061414f6f060f0b7fe4
declannoble <declanjnoble@hotmail.com>
2022-05-04 20:39:48 -0700

    build: adding to check

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list.
+ * add_dnodeint_end - adds at new node at the end of a dlistint_t list
  * @head: pointer to the beginning of the list
  * @n: integer
   * Return: pointer to the new node
  */
-listint_t *add_nodeint_end(listint_t **head, const int n)
+listint_t *add_dnodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *current;
