7a4cd2bfe6f7d2a560be3f1b91dd2a0cf8d0b238
declannoble <97944678+declannoble@users.noreply.github.com>
2022-05-05 10:09:56 +1000

    build: initial commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints elements of a list
+ *@h: list to be printed
+ *Return: count of elements
+ */
+
+size_t print_dlistint(const dlistint_t *h);
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
