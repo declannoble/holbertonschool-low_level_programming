27850b476251881b352a20c82935ce401ac8068f
declannoble <declanjnoble@hotmail.com>
2022-05-04 19:20:35 -0700

    style: betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * print_listint - prints elements of a list
+ * print_dlistint - prints elements of a list
  *@h: list to be printed
  *Return: count of elements
  */
 
-size_t print_dlistint(const dlistint_t *h);
+size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
 
