770ac148e5298bffb4e78fa160fac51b0c4a8130
declannoble <declanjnoble@hotmail.com>
2022-05-05 01:46:35 -0700

    build: reverted back

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		new->next = *h;
 		*h = new;
-		new->prev = NULL;
+		return (new);
 	}
 	new->next = current->next;
 	current->next = new;
