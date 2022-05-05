42e6ae11881340ec4472a8011b25153a44b733d3
declannoble <97944678+declannoble@users.noreply.github.com>
2022-05-05 11:00:33 +1000

    build: removing semicolon from prototype

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,1 +1,20 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - count the number of nodes in the linked list
+ * @h: pointer to the beginning of a linked list
+ *
+ * Return: number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 
