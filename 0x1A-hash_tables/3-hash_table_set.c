714d9362d47bee2ee0052a1bff25b46091f6a054
declannoble <declanjnoble@hotmail.com>
2022-05-26 20:56:27 -0700

    3-hash_table_set.c

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds an element(key, value) into a hash table
+ * @ht: a pointer to the hash table
+ * @key: the key
+ * @value: the value
+ * Return: 1 on success, 0 on failure
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *newnode = NULL;
+	hash_node_t *current = NULL;
+
+	if (ht == NULL || ht->array == NULL || key == NULL)
+		return (0);
+
+	index = key_index(key, value);
+	current = ht->array[index];
+
+	while (current != NULL)
+	{
+		if (strcmp(key, current->key) == 0)
+		{
+			free(current->value);
+			current->value = strdup(value);
+			return (1);
+		}
+		current = current->next;
+	}
+
+	newnode = malloc(sizeof(*newnode));
+	if (newnode == NULL)
+		return (0);
+	newnode->key = strdup(key);
+	newnode->value = strdup(value);
+	newnode->next = ht->array[index];
+	ht->array[index] = newnode;
+	return (1);
+}
