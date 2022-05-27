a265dbec958eab20506a8e41b61321a09d5cddd4
declannoble <declanjnoble@hotmail.com>
2022-05-26 21:46:33 -0700

    fixed cast of index

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || ht->array == NULL || key == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *)key, value);
+	index = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[index];
 
 	while (current != NULL)
